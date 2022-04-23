e73889b36fbbb3fcbdad770ceb4a682c0ddba460
Rahele <raheleh.tabasi@gmail.com>
2022-04-23 14:41:24 +0430

    create role & User & UserRole model

---
+++ LearnWebsite.Data/Entities/User/Role.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWebsite.Data.Entities.User
+{
+    public class Role
+    {
+        public Role()
+        {
+
+        }
+
+        [Key]
+        public int RoleId { get; set; }
+
+        [Display(Name ="عنوان نقش")]
+        [MaxLength(200,ErrorMessage ="{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+        public string RoleTitle { get; set; }
+
+
+        public virtual IEnumerable<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ LearnWebsite.Data/Entities/User/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWebsite.Data.Entities.User
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        [Key]
+        public int UserId { get; set; }
+
+        [Display(Name = "نام کاربری")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+        public string UserName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "رمزعبور")]
+        [MinLength(5, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+        public string Password { get; set; }
+
+        [Display(Name = "کد فعال سازی")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string ActivateCode { get; set; }
+
+        [Display(Name = "وضعیت فعال بودن")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "آواتار")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string UserAvatar { get; set; }
+
+        [Display(Name = "تاریخ ثبت نام")]
+        public DateTime RegisterDate { get; set; }
+
+
+        public virtual IEnumerable<UserRole> UserRoles { get; set; }
+    }
+}

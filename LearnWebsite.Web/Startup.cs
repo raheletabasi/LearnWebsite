937b2edcd15584fe13a27a4664e23fd1c04b7ab7
Rahele <raheleh.tabasi@gmail.com>
2022-04-24 12:23:43 +0430

    add setting for startup

--- LearnWebsite.Web/Startup.cs
+++ LearnWebsite.Web/Startup.cs
@@ -16,6 +16,8 @@ namespace LearnWebsite.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +27,13 @@ namespace LearnWebsite.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                        name : "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

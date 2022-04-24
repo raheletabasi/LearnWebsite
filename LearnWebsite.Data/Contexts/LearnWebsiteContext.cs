7daad2dd9be292bae3a3e3ffe105e0ecbc90084b
Rahele <raheleh.tabasi@gmail.com>
2022-04-24 13:42:06 +0430

    add context and connection string setting

---
+++ LearnWebsite.Data/Contexts/LearnWebsiteContext.cs
@@ -0,0 +1,22 @@
+﻿using LearnWebsite.Data.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWebsite.Data.Contexts
+{
+    public class LearnWebsiteContext : DbContext
+    {
+        public LearnWebsiteContext(DbContextOptions<LearnWebsiteContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}

bdba25383519b453575a07af724d2e724912a7d4
Rahele <raheleh.tabasi@gmail.com>
2022-04-28 17:35:49 +0430

    add User panel area for view information

--- LearnWebsite.Core/DTOs/AccountViewModel.cs
+++ LearnWebsite.Core/DTOs/AccountViewModel.cs
@@ -8,31 +8,31 @@ using System.Threading.Tasks;
 namespace LearnWebsite.Core.DTOs
 {
     public class RegisterViewModel
-    {
-        [Display(Name = "نام کاربری")]
-        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
-        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
-        public string UserName { get; set; }
+        {
+            [Display(Name = "نام کاربری")]
+            [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+            [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+            public string UserName { get; set; }
 
-        [Display(Name = "ایمیل")]
-        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
-        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
-        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
-        public string Email { get; set; }
+            [Display(Name = "ایمیل")]
+            [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+            [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+            [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+            public string Email { get; set; }
 
-        [Display(Name = "کلمه عبور")]
-        [MinLength(5, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
-        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
-        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
-        public string Password { get; set; }
+            [Display(Name = "کلمه عبور")]
+            [MinLength(5, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
+            [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+            [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+            public string Password { get; set; }
 
-        [Display(Name = "تکرار کلمه عبور")]
-        [MinLength(5, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
-        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
-        [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
-        [Compare("Password", ErrorMessage = "تکرار کلمه عبور با کلمه عبور یکسان نمی باشد")]
-        public string RePassword { get; set; }
-    }
+            [Display(Name = "تکرار کلمه عبور")]
+            [MinLength(5, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
+            [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+            [Required(ErrorMessage = "تمکیل نمودن فیلد {0} الزامی می باشد")]
+            [Compare("Password", ErrorMessage = "تکرار کلمه عبور با کلمه عبور یکسان نمی باشد")]
+            public string RePassword { get; set; }
+        }
 
     public class LoginViewModel
     {
---
+++ LearnWebsite.Web/Areas/UserPanel/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿
+using LearnWebsite.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnWebsite.Web.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class HomeController : Controller
+    {
+        IUserService _userService;
+
+        public HomeController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View(_userService.GetUserInformation(User.Identity.Name));
+        }
+    }
+}

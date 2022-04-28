54d354eb3c6c851d2399a44871e6ea17bf4991a7
Rahele <raheleh.tabasi@gmail.com>
2022-04-28 17:25:31 +0430

    add convertor for date

---
+++ LearnWebsite.Core/Utility/Convertor/ConvertDate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace LearnWebsite.Core.Utility.Convertor
+{
+    public static class ConvertDate
+    {
+        public static string ToShamsi(this DateTime dateTime)
+        { 
+            PersianCalendar persianCalendar = new PersianCalendar();
+            return persianCalendar.GetYear(dateTime).ToString() +
+                   persianCalendar.GetMonth(dateTime).ToString("00") +
+                   persianCalendar.GetDayOfMonth(dateTime).ToString("00");
+        }
+    }
+}

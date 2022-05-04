9b1d99027268871c311d6eb9fff96453b3dd47b3
Rahele <raheleh.tabasi@gmail.com>
2022-05-04 23:46:53 +0430

    add dbset and seed data for cashwallet

--- LearnWebsite.Data/Contexts/LearnWebsiteContext.cs
+++ LearnWebsite.Data/Contexts/LearnWebsiteContext.cs
@@ -1,4 +1,5 @@
 ﻿using LearnWebsite.Data.Entities.User;
+using LearnWebsite.Data.Entities.CashWallet;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,14 +16,25 @@ namespace LearnWebsite.Data.Contexts
 
         }
 
+        #region user
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
+        #endregion
+
+        #region cashWallet
+        public DbSet<CashType> CashTypes { get; set; }
+        public DbSet<CashWallet> CashWallets { get; set; }
+        #endregion
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UserRole>().HasKey(e => new { e.RoleId, e.UserId });
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CashType>().HasData(
+                new CashType { CashTypeId = 1, TypeTitle = "واریز" },
+                new CashType { CashTypeId = 2, TypeTitle = "برداشت"});
         }
     }
 }

c4e7b3b9745808f5ab8105de8dee175b35d385ed
Rahele <raheleh.tabasi@gmail.com>
2022-05-06 19:33:45 +0430

    add service for charge cash wallet and create view for wallet

---
+++ LearnWebsite.Web/Areas/UserPanel/Controllers/CashWalletController.cs
@@ -0,0 +1,37 @@
+﻿using LearnWebsite.Core.DTOs;
+using LearnWebsite.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnWebsite.Web.Areas.UserPanel.Controllers
+{
+    [Authorize]
+    [Area("UserPanel")]    
+    public class CashWalletController : Controller
+    {
+        IUserService _userService;
+
+        public CashWalletController(IUserService userService) => _userService = userService;
+
+        [Route("UserPanel/CashWallet")]
+        public IActionResult CashWallet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UserPanel/CashWallet")]
+        public IActionResult CashWallet(ChargeCashWalletViewModel chargeCashWalletViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["ListOfCharge"] = _userService.GetHistoryCashWallet(User.Identity.Name);
+                return View(chargeCashWalletViewModel);
+            }
+
+            _userService.ChargeCashWallet(User.Identity.Name, chargeCashWalletViewModel.Amount);
+
+            return View();
+        }
+    }
+}
